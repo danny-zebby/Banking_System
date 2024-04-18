2114b62aa4a10120afe109e2b83330f58d17d616
BrLoo <111550672+BrLoo@users.noreply.github.com>
2024-04-18 14:35:30 -0700

    Added to Deposit, Withdraw, and Transfer

--- src/main/ATM.java
+++ src/main/ATM.java
@@ -1,10 +1,15 @@
 import java.io.*;
+import java.util.*;
 
 public class ATM {
 	private float money;
 	private int admin;
+	// temporary
 	private BankUser user;
 	private BankUser account;
+	private BankAccount accountPin;
+	private BankAccount balance;
+	Scanner scan;
 	
 	public void setMoney(float money) {
 		 this.money = money;
@@ -31,15 +36,60 @@ public class ATM {
 	}
 	
 	public void deposit(float money) {
-		money += account;
-		// insert code here
+		// Console purposes, may change once GUI is implemented
+		scan = new Scanner(System.in);
+		System.out.println("Enter Amount: ");
+		String num = scan.nextLine();
+		money = Float.parseFloat(num);
+		System.out.println("Enter pin: ");
+		String pin = scan.nextLine();
+		if (pin == ) {
+			money += account;
+			System.out.println("Transaction Successful.");
+		}
+		else {
+			System.out.println("Error, pin is incorrect. Please try again.");
+		}
 	}
 	
 	public void withdraw() {
-		// insert code here
+		// Console purposes, may change once GUI is implemented
+		scan = new Scanner(System.in);
+		System.out.println("Enter Amount: ");
+		String num = scan.nextLine();
+		money = Float.parseFloat(num);
+		System.out.println("Enter pin: ");
+		String pin = scan.nextLine();
+		if (pin == accountPin) {
+			money -= account;
+			System.out.println("Transaction Successful");
+		}
+		else {
+			System.out.println("Error, pin is incorrect. Please try again.");
+		}
 	}
 	
 	public void transfer() {
-		// insert code here
+		scan = new Scanner(System.in);
+		System.out.println("Enter Receiver's Account Number: ");
+		String accnum = scan.nextLine();
+		
+		System.out.println("Enter Amount: ");
+		String num = scan.nextLine();
+		money = Float.parseFloat(num);
+		System.out.println("Is this correct?" + "\n" + accnum + "\n" + balance + "\n");
+		String text = scan.nextLine();
+		text.toUpperCase();
+		if (text == "YES") {
+			System.out.println("Enter pin: ");
+			String pin = scan.nextLine();
+			if (pin == ) {
+				System.out.println("Transaction Successful");
+			} else {
+				System.out.println("Error, pin is incorrect. Please try again.");
+			}
+		} else {
+			transfer();
+		}
 	}
 }

afea622e5559420367ae81e4d56edc10528fed4b
BrLoo <111550672+BrLoo@users.noreply.github.com>
2024-04-18 13:28:11 -0700

    Add files via upload

---
+++ src/main/ATM.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+public class ATM {
+	private float money;
+	private int admin;
+	private BankUser user;
+	private BankUser account;
+	
+	public void setMoney(float money) {
+		 this.money = money;
+	}
+	
+	public void setAdmin(int admin) {
+		this.admin = admin;
+	}
+	
+	public void setUser(BankUser user) {
+		this.user = user;
+	}
+	
+	public float getMoney() {
+		return this.money;
+	}
+	
+	public int getAdmin() {
+		return this.admin;
+	}
+	
+	public void getUser() {
+		return this.user;
+	}
+	
+	public void deposit(float money) {
+		money += account;
+	}
+	
+	public void withdraw() {
+		
+	}
+	
+	public void transfer() {
+		
+	}
+}
